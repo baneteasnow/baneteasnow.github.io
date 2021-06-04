cf4f2e045ac2f1669d44c56c292abd0a9acf1756
baneteasnow <81329385+baneteasnow@users.noreply.github.com>
2021-06-04 16:15:25 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,37 +2,35 @@
 
 var cacheVersion = 1;
 var currentCache = {
-  offline: 'offline-cache' + cacheVersion
+   offline: 'offline-cache' + cacheVersion
 };
-const offlineUrl = 'offline.html';
+
+// Page you want to cache
+const offlinePage = ‘offline.html';
 
 this.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(currentCache.offline).then(function(cache) {
-      return cache.addAll([
-          offlineUrl
-      ]);
-    })
-  );
+   event.waitUntil(
+      caches.open(currentCache.offline).then(function(cache) {
+         return cache.addAll([offlinePage]);
+      })
+   );
 });
 
+// Now to retrieve the cached pages, we will use the fetch event.
 this.addEventListener('fetch', event => {
-  // request.mode = navigate isn't supported in all browsers
-  // so include a check for Accept: text/html header.
-  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-        event.respondWith(
-          fetch(event.request.url).catch(error => {
-              // Return the offline page
-              return caches.match(offlineUrl);
-          })
-    );
-  }
-  else{
-        // Respond with everything else if we can
-        event.respondWith(caches.match(event.request)
-                        .then(function (response) {
-                        return response || fetch(event.request);
-                    })
-            );
-      }
+   if(event.request.mode === 'navigate' || (event.request.method === 'GET')) {
+      event.respondWith(
+         fetch(event.request.url).catch(error => {
+            // Return the offline page
+            return caches.match(offlinePage);
+         })
+      );
+   }
+   else{
+      event.respondWith(
+         caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+         })
+      );
+   }
 });
